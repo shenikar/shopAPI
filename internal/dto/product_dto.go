61eb3665d02b916fdde2debb779e0ba3f6bd7b4f
shenikar <shenikar@student.21-school.ru>
2025-10-14 22:01:35 +0300

    fix struct

--- internal/dto/product_dto.go
+++ internal/dto/product_dto.go
@@ -1,14 +1,12 @@
 package dto
 
-import "github.com/google/uuid"
 
 type CreateProductDTO struct {
-	Name           string    `json:"name" validate:"required"`
-	Category       string    `json:"category" validate:"required"`
-	Price          float64   `json:"price" validate:"required,gt=0"`
-	AvailableStock int       `json:"available_stock" validate:"required,gte=0"`
-	SupplierID     int       `json:"supplier_id" validate:"required"`
-	ImageID        uuid.UUID `json:"image_id" validate:"required"`
+	Name           string  `json:"name" validate:"required"`
+	Category       string  `json:"category" validate:"required"`
+	Price          float64 `json:"price" validate:"required,gt=0"`
+	AvailableStock int     `json:"available_stock" validate:"required,gte=0"`
+	SupplierID     string  `json:"supplier_id" validate:"required,uuid"`
 }
 
 type ProductResponseDTO struct {
@@ -18,6 +16,6 @@ type ProductResponseDTO struct {
 	Price          float64 `json:"price"`
 	AvailableStock int     `json:"available_stock"`
 	LastUpdateDate string  `json:"last_update_date"`
-	SupplierID     int     `json:"supplier_id"`
+	SupplierID     string  `json:"supplier_id"`
 	ImageID        string  `json:"image_id"`
 }
--- internal/dto/supplier_dto.go
+++ internal/dto/supplier_dto.go
@@ -1,14 +1,14 @@
 package dto
 
 type CreateSupplierDTO struct {
-	Name        string `json:"name" validate:"required"`
-	AddressID   string `json:"address_id" validate:"required"`
-	PhoneNumber string `json:"phone_number" validate:"required"`
+	Name        string           `json:"name" validate:"required"`
+	Address     CreateAddressDTO `json:"address" validate:"required"`
+	PhoneNumber string           `json:"phone_number" validate:"required"`
 }
 
 type SupplierResponseDTO struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
-	AddressID   string `json:"address_id"`
-	PhoneNumber string `json:"phone_number"`
+	Address     AddressResponseDTO `json:"address"`
+	PhoneNumber string             `json:"phone_number"`
 }
